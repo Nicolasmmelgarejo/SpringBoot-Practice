0d17912ce43c10940e5e1de15f05b8e496931b80
Nicolasmmelgarejo <nicolasm.melgarejo@gmail.com>
2022-05-31 21:43:25 -0300

    added swagger

--- src/main/java/com/practice/springboot/controller/RestController.java
+++ src/main/java/com/practice/springboot/controller/RestController.java
@@ -1,19 +1,34 @@
 package com.practice.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.practice.springboot.model.ContactModel;
+import com.practice.springboot.constant.ViewConstant;
+import com.practice.springboot.service.ContactService;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest")
 public class RestController {
 	
+	@Autowired
+	@Qualifier("contactServiceImpl")
+	private ContactService contactService;
+	
 	@GetMapping("/checkrest")
-	public ResponseEntity<ContactModel> checkRest(){
-		ContactModel cm= new ContactModel(2,"perez","mikel","1140928472","lomas");
-		return new ResponseEntity<ContactModel>(cm,HttpStatus.OK);
+	public ResponseEntity<ModelAndView> checkRest(){
+		ModelAndView mav = new ModelAndView(ViewConstant.CONTACTS);
+		mav.addObject("contacts", contactService.listAllContacts());
+		return new ResponseEntity<ModelAndView>(mav,HttpStatus.OK);
+	}
+	@GetMapping("/removecontact")
+	public ResponseEntity<ModelAndView> removeContact(@RequestParam(name="id",required=true) int id) {
+		contactService.removeContact(id);
+		return checkRest();
 	}
 }
